f8934278983b9458b0e7c4156af6a0ed44b79bb2
AbdullahKhn <abdullahkhn@gmail.com>
2014-09-19 13:24:05 +0500

    Contains the R-Script

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,25 @@
+#"read in and combine the training set with the activity codes and subject id"
 train <- read.table("train/X_train.txt")
 train_act <- read.table("train/y_train.txt")
 train_obs <- read.table("train/subject_train.txt")
 train_data <- cbind(train_obs,train_act,train)
 
+#"read in and combine the test set with the activity codes and subject id"
 test <- read.table("test/X_test.txt")
 test_act <- read.table("test/y_test.txt")
 test_obs <- read.table("test/subject_test.txt")
 test_data <- cbind(test_obs,test_act,test)
 
+#"Bind the two sets together"
 data <- rbind(train_data,test_data)
 
+#"read in the feature varaible names and name the data set"
 features <- read.table("features.txt")
 labels <- as.vector(features[['V2']])
 label_obs_act <- c("ID_Obs","Activity",labels)
 colnames(data) <- label_obs_act
 
+#"select the required feature variables"
 library(dplyr)
 data_m_sd  <- select(data,contains("ID_Obs"),contains("Activity"),contains("-mean()"),contains("-std()"))
 data_m_sd <- data_m_sd[,-(26:28)]
@@ -25,12 +30,14 @@ data_m_sd <- data_m_sd[,-34]
 data_m_sd <- data_m_sd[,-35]
 data_m_sd <- data_m_sd[,-36]
 
+#"clean the variable names and label the activity codes"
 colnames(data_m_sd) <- gsub("\\(\\)","",names(data_m_sd),)
 colnames(data_m_sd) <- gsub("-","_",names(data_m_sd),)
 act <- read.table("activity_labels.txt")
 activityname <- as.vector(act[['V2']])
 data_m_sd$Activity <- factor(data_m_sd$Activity, levels = c(1:6), labels = c(activityname))
  
+#"reshape the data using the melt and casting functions"
 library(reshape2)
 variables <- names(data_m_sd)
 variables <- variables[3:68]
@@ -44,4 +51,9 @@ for (i in 1:30){
 	data_tidy <- rbind(data_tidy,dtcast_obs)
 }
 names(data_tidy)[1] <- paste("ID_Obs")
+
+#"export the data into text format"
+write.table(data_tidy,"r_analysis_data.txt", row.name = F)
+
+
 
