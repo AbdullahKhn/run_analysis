3b360782e887e7caef5400b1eb9910fd0b6392c7
AbdullahKhn <abdullahkhn@gmail.com>
2014-09-19 15:19:44 +0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,12 +16,12 @@ data <- rbind(train_data,test_data)
 #"read in the feature varaible names and name the data set"
 features <- read.table("UCI HAR Dataset/features.txt")
 labels <- as.vector(features[['V2']])
-label_obs_act <- c("ID_Obs","Activity",labels)
+label_obs_act <- c("ID_Sub","Activity",labels)
 colnames(data) <- label_obs_act
 
 #"select the required feature variables"
 library(dplyr)
-data_m_sd  <- select(data,contains("ID_Obs"),contains("Activity"),contains("-mean()"),contains("-std()"))
+data_m_sd  <- select(data,contains("ID_Sub"),contains("Activity"),contains("-mean()"),contains("-std()"))
 data_m_sd <- data_m_sd[,-(26:28)]
 data_m_sd <- data_m_sd[,-(29:31)]
 data_m_sd <- data_m_sd[,-(32:34)]
@@ -41,16 +41,16 @@ data_m_sd$Activity <- factor(data_m_sd$Activity, levels = c(1:6), labels = c(act
 library(reshape2)
 variables <- names(data_m_sd)
 variables <- variables[3:68]
-data_melt <- melt(data_m_sd,id=c("ID_Obs","Activity"),measure.vars=c(variables))
+data_melt <- melt(data_m_sd,id=c("ID_Sub","Activity"),measure.vars=c(variables))
 
 data_tidy <- data.frame()
 for (i in 1:30){
-	dt <- data_melt[data_melt$ID_Obs == i,]
+	dt <- data_melt[data_melt$ID_Sub == i,]
 	dtcast <- dcast(dt, Activity ~ variable,mean)
 	dtcast_obs <- cbind(i, dtcast)
 	data_tidy <- rbind(data_tidy,dtcast_obs)
 }
-names(data_tidy)[1] <- paste("ID_Obs")
+names(data_tidy)[1] <- paste("ID_Sub")
 
 #"export the data into text format"
 write.table(data_tidy,"r_analysis_data.txt", row.name = F)
